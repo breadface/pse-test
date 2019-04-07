197c9304ef05bf81311def21e01b9526d4107743
Omonori Jesmine <damilolao328@gmail.com>
2019-04-07 23:10:10 +0100

    Updates

---
+++ precache-manifest.2bea1ed4e99184bbc16034d4a8980f70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13d6c005651e0eaf6ae2",
+    "url": "/pse-test/static/js/runtime~main.4f95a7da.js"
+  },
+  {
+    "revision": "d2f0475a88aa8ad4cc52",
+    "url": "/pse-test/static/js/main.b5fe3efe.chunk.js"
+  },
+  {
+    "revision": "b5b2f6114a7930789ce6",
+    "url": "/pse-test/static/js/2.ed78f4ed.chunk.js"
+  },
+  {
+    "revision": "d2f0475a88aa8ad4cc52",
+    "url": "/pse-test/static/css/main.4d30b66d.chunk.css"
+  },
+  {
+    "revision": "e232ac973aafb3d6e7557b981c6e3c45",
+    "url": "/pse-test/index.html"
+  }
+];
--- src/components/shared/NoMatch.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
-
-const NoMatch = () => <Redirect to="/" />;
-
-export default NoMatch;
